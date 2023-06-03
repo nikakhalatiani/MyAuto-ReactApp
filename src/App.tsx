1d26a1473d9826a522663cddd7a77968dd889196
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2023-06-03 17:06:53 +0400

    Added loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import './App.css';
+import Loading from "./Loading/Loading";
+import { useState, useEffect } from "react";
+
+const mans_list = "https://static.my.ge/myauto/js/mans.json";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  async function fetchMans() {
+    setLoading(true);
+    try {
+      const response = await fetch(mans_list);
+      const mans = await response.json();
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log("error");
+    }
+  }
+
+  useEffect(() => {
+    fetchMans();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      Hi     
-    </div>
+    <h2>hello world</h2>
   );
 }
 
---
+++ src/Loading/Loading.tsx
@@ -0,0 +1,74 @@
+import './Loading.css';
+
+export default function Loading() {
+    return (
+      <div className="Loading">
+         <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          r="7.146"
+          fill="none"
+          stroke="#FD4100"
+          strokeWidth="3"
+        >
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;40"
+            keyTimes="0;1"
+            keySplines="0 0.2 0.8 1"
+            calcMode="spline"
+            begin="-0.5s"
+          />
+          <animate
+            attributeName="opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="1;0"
+            keyTimes="0;1"
+            keySplines="0.2 0 0.8 1"
+            calcMode="spline"
+            begin="-0.5s"
+          />
+        </circle>
+        <circle
+          cx="50"
+          cy="50"
+          r="29.011"
+          fill="none"
+          stroke="#ff5d00"
+          stroke-width="3"
+        >
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;40"
+            keyTimes="0;1"
+            keySplines="0 0.2 0.8 1"
+            calcMode="spline"
+          />
+          <animate
+            attributeName="opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="1;0"
+            keyTimes="0;1"
+            keySplines="0.2 0 0.8 1"
+            calcMode="spline"
+          />
+        </circle>
+      </svg>{" "}
+      </div>
+    );
+  }
+  
+  
