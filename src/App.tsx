a2ac10f5b58b86f2914df7e7cb741210f554e6b6
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2023-06-04 13:24:01 +0400

    added search-dropdown-meny

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Loading from "./Loading/Loading";
+import SearchDrop from "./Components/SearchDropdown";
 import { useState, useEffect } from "react";
 
 const mans_list = "https://static.my.ge/myauto/js/mans.json";
@@ -7,9 +8,11 @@ const mans_list = "https://static.my.ge/myauto/js/mans.json";
 function App() {
   const [loading, setLoading] = useState(true);
 
+
   async function fetchMans() {
     setLoading(true);
     try {
+      // await new Promise((resolve) => setTimeout(resolve, 3000)); // simulate slow network by waiting 3 seconds
       const response = await fetch(mans_list);
       const mans = await response.json();
       setLoading(false);
@@ -31,8 +34,25 @@ function App() {
     );
   }
 
+  const options = [
+    { value: 'option1', label: 'Jeep' },
+    { value: 'option2', label: 'Benzo' },
+    { value: 'option3', label: 'Optio'},
+    { value: 'option4', label: 'Baza'},
+    { value: 'option5', label: 'Caza'},
+    { value: 'option6', label: 'Daza'},
+    { value: 'option7', label: 'Eaza'},
+    { value: 'option8', label: 'Faza'},
+    { value: 'option9', label: 'yaza'},
+    { value: 'option10', label: 'zaza'},
+
+
+    
+
+  ];
+
   return (
-    <h2>hello world</h2>
+    <SearchDrop options={options}/>
   );
 }
 
---
+++ src/Components/SearchDropdown.tsx
@@ -0,0 +1,187 @@
+import "./SearchDropdown.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronUp,
+  faXmark,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface SearchDropdownProps {
+  options: Option[];
+}
+
+const SearchDropdown: React.FC<SearchDropdownProps> = ({ options }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [isCloseButtonSelected, setIsCloseButtonSelected] = useState(false);
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const toggleDropdown = () => {
+    if (isCloseButtonSelected) {
+      setSelectedOptions([]);
+      setIsCloseButtonSelected(false);
+    } else {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  const handleCheckboxChange = (option: Option) => {
+    const isSelected = selectedOptions.some(
+      (selectedOption) => selectedOption.value === option.value
+    );
+
+    if (isSelected) {
+      setSelectedOptions(
+        selectedOptions.filter(
+          (selectedOption) => selectedOption.value !== option.value
+        )
+      );
+      if (selectedOptions.length === 1 && isCloseButtonSelected) {
+        setIsCloseButtonSelected(false);
+      }
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+      if (!isCloseButtonSelected) {
+        setIsCloseButtonSelected(true);
+      }
+    }
+
+    if (searchInputRef.current) {
+      searchInputRef.current.placeholder = selectedOptions
+        .map((selectedOption) => selectedOption.label)
+        .join(", ");
+    }
+  };
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleClearAll = () => {
+    setSelectedOptions([]);
+    setIsCloseButtonSelected(false);
+    if (searchInputRef.current) {
+      searchInputRef.current.placeholder = "Search";
+    }
+  };
+
+  const handleCloseWithSelection = () => {
+    setIsOpen(false);
+    setSearchTerm("");
+  };
+
+  const filteredOptions = options.filter((option) =>
+    option.label.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <div
+        className={`search-container ${
+          selectedOptions.length > 0 ? "dark-font" : ""
+        } ${isOpen ? "open" : ""}`}
+      >
+        {" "}
+        <input
+          onClick={() => setIsOpen(true)}
+          placeholder={
+            selectedOptions.length > 0
+              ? selectedOptions
+                  .map((selectedOption) => selectedOption.label)
+                  .join(", ")
+              : "Search"
+          }
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          ref={searchInputRef}
+        />
+        <button
+          onClick={toggleDropdown}
+          className={
+            isCloseButtonSelected ? "rotate-x" : isOpen ? "rotate" : ""
+          }
+        >
+          {isCloseButtonSelected ? (
+            <FontAwesomeIcon icon={faXmark} style={{ color: "#272a37" }} />
+          ) : isOpen ? (
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              style={{ color: "#6f7383",  height: "10px", width: "10px" }}
+            />
+          ) : (
+            <FontAwesomeIcon
+            icon={faChevronDown}
+            style={{ color: "#6f7383" ,  height: "10px", width: "10px"  }}
+          />          )}
+        </button>
+      </div>
+      <div>
+        {isOpen && (
+          <div className="dropdown">
+            <div className="dropdown-options">
+              {filteredOptions.length === 0 ? (
+                <span>No records</span>
+              ) : (
+                filteredOptions.map((option) => (
+                  <label key={option.value}>
+                    <input
+                      type="checkbox"
+                      checked={selectedOptions.some(
+                        (selectedOption) =>
+                          selectedOption.value === option.value
+                      )}
+                      onChange={() => {
+                        handleCheckboxChange(option);
+                        setIsChecked(!isChecked);
+                      }}
+                    />
+                    {selectedOptions.some(
+                      (selectedOption) => selectedOption.value === option.value
+                    ) ? (
+                      <span className="custom-checkbox-checked">
+                        <FontAwesomeIcon
+                          icon={faCheck}
+                          style={{ height: "10px", width: "10px" }}
+                        />
+                      </span>
+                    ) : (
+                      <span className="custom-checkbox-unchecked"></span>
+                    )}
+                    {option.label}
+                  </label>
+                ))
+              )}
+            </div>
+
+            {selectedOptions.length > 0 && (
+              <div className="dropdown-buttons">
+                <button className="clear-button" onClick={handleClearAll}>
+                  Clear Filter
+                </button>
+                <button
+                  className="close-button"
+                  onClick={handleCloseWithSelection}
+                >
+                  Choose
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchDropdown;
